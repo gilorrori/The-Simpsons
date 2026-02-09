10507f60e20663bf18d2aa22e547d87a6a31d6da
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-02-09 09:05:16 -0600

    Se agregan elementos al item del personaje con animaciones lottie

--- app/src/main/java/com/gilorroristore/thesimpsons/ui/characters/adapter/CharacterViewHolder.kt
+++ app/src/main/java/com/gilorroristore/thesimpsons/ui/characters/adapter/CharacterViewHolder.kt
@@ -1,9 +1,11 @@
 package com.gilorroristore.thesimpsons.ui.characters.adapter
 
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.gilorroristore.thesimpsons.BuildConfig
 import com.gilorroristore.thesimpsons.R
+import com.gilorroristore.thesimpsons.core.enum.StatusCharacter
 import com.gilorroristore.thesimpsons.databinding.ItemCharacterBinding
 import com.gilorroristore.thesimpsons.domain.model.CharacterDetailModel
 
@@ -12,13 +14,29 @@ class CharacterViewHolder(
     private val onItemSelected: (Int) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    /**
+     * Pinta toda la información del personaje en cada Item
+     * del RecyclerView
+     * @author gil_orrori_94@hotmail.com
+     */
     fun addItem(characterDetail: CharacterDetailModel) {
         val context = binding.root.context
         val base = BuildConfig.BASE_URL_IMAGE
 
         Glide.with(context).load(base + characterDetail.portraitPath).into(binding.ivCharacter)
         binding.tvCharacterName.text = characterDetail.name
-        binding.tvOccupation.text = context.getString(R.string.character_occupation, characterDetail.occupation)
+        binding.tvOccupation.text = characterDetail.occupation
+        val status = characterDetail.status
+
+        if (status.contains(StatusCharacter.Alive.toString())){
+            binding.ltAlive.setAnimation(R.raw.live_character)
+            binding.tvStatusCharacter.text = context.getString(R.string.generic_alive)
+        } else if (status.contains(StatusCharacter.Deceased.toString())) {
+            binding.ltAlive.setAnimation(R.raw.deceased_character)
+            binding.tvStatusCharacter.text = context.getString(R.string.generic_deceased)
+        } else {
+            binding.ltAlive.isVisible = false
+        }
 
         binding.root.setOnClickListener {
             onItemSelected(characterDetail.id)
